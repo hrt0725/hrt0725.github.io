8ab248fe38fb26d0a4a9b5d9ed42a69b70e44c1b
hrt0725@163.com <hrt0725@163.com>
2025-11-21 13:55:59 +0800

    updata script

--- assets/channel-BFeXsBGJ.js
+++
@@ -1,1 +0,0 @@
-import{U as a,C as n}from"./index-Bc2kBZfk.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
--- hgi/script/gmd.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-rmdir /s /q "..\assets" 
-
-set "musicDir=..\music"
-set "output=.\public\data\musics.json"
-
-powershell -NoProfile -Command  "Get-ChildItem -Path '%musicDir%' -File | Where-Object { $_.Name -ne 'list.json' } | Sort-Object Name | ForEach-Object { $_.Name } | ConvertTo-Json -Depth 1 | Set-Content -Path '%output%' -Encoding UTF8"
